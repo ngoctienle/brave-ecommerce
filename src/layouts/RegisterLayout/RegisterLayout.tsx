4d73481671e58e37674db428ca8291a692e26c4a
Le Ngoc Tien <94373805+ngoctienle@users.noreply.github.com>
2022-11-28 23:06:52 +0700

    feat(route): config route with react-router

---
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -0,0 +1,7 @@
+interface Props {
+  children?: React.ReactNode
+}
+
+export default function RegisterLayout({ children }: Props) {
+  return <div>RegisterLayout {children}</div>
+}
---
+++ src/useRouteElements.tsx
@@ -0,0 +1,27 @@
+import Login from './pages/Login'
+import ProductList from './pages/ProductList'
+import Register from './pages/Register'
+import { useRoutes } from 'react-router-dom'
+import RegisterLayout from './layouts/RegisterLayout'
+
+export default function useRouteElements() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: <Register />
+    }
+  ])
+  return routeElements
+}
