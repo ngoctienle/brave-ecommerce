08d5cc196fe3ef085afaebf7ff23c3e5fc04d0af
Le Ngoc Tien <94373805+ngoctienle@users.noreply.github.com>
2022-11-29 21:24:50 +0700

    style(*): add container component tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
   theme: {
     extend: {
       colors: {
@@ -8,5 +14,17 @@ module.exports = {
       }
     }
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.container': {
+          maxWidth: theme('columns.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4')
+        }
+      })
+    })
+  ]
 }
