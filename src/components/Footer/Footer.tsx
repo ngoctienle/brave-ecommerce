cd3258a5420d9c44a9956b7ec685396e3ea45562
Le Ngoc Tien <94373805+ngoctienle@users.noreply.github.com>
2022-11-29 21:26:23 +0700

    style(*): using container class

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className='bg-neutral-100 py-16'>
-      <div className='mx-auto max-w-7xl px-4'>
+      <div className='container'>
         <div className='grid grid-cols-1 gap-4 lg:grid-cols-3'>
           <div className='lg:col-span-1'>
             <span>© 2022 Shopee. Tất cả các quyền được bảo lưu.</span>
--- src/components/RegisterHeader/RegisterHeader.tsx
+++ src/components/RegisterHeader/RegisterHeader.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function RegisterHeader() {
   return (
     <header className='py-5'>
-      <div className='mx-auto max-w-7xl px-4'>
+      <div className='container'>
         <nav className='flex items-end'>
           <Link to='/'>
             <svg viewBox='0 0 192 65' className='h-8 fill-orange lg:h-11'>
